cec759b098e06a4250903118e6956951a4f92f9c
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2021-10-28 00:38:39 -0400

    add switch for recognizing encode or decode mode

--- main.py
+++ main.py
@@ -2,4 +2,10 @@
 
 import sys
 
-print("arguuments:\t", sys.argv[1:])
+print("arguuments:\t", sys.argv[1:], "\n")
+
+argument = sys.argv[1]
+if argument == "d":
+    print("decode mode")
+elif argument == "e":
+    print("encode mode")
