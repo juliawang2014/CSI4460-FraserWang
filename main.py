33483234a1c52038026c9bd6e8bb6297650ca9ab
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2021-10-28 00:47:29 -0400

    add eyes.png for later image stuff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+#first argument is gonna be e or d for Encode or Decode for now, then some sort of file input, gonna use a png image in local folder for now.
+
 import sys
 
 print("arguuments:\t", sys.argv[1:], "\n")
