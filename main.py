a5c8d3225ea844e1476cba4fa451cbf2aff11585
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2021-11-04 23:46:53 -0400

    add code to encode data in image

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import sys
 
 print("arguments:\t", sys.argv[1:], "\n")
 
-def printModeTest(mode):
+def printModeTest(mode, text):
     if mode == "e" or mode == "encode":
         print("encode")
-        openImage()
+        openImage(text)
     elif mode == "d" or mode == "decode":
         print("decode")
         
-def openImage():
+def openImage(text):
     with Image.open("./media/eyes.png") as image:
         #output first 10 pixels, don't want to completely clear the console output.
         imageArray = list(image.getdata(band=None))
@@ -28,8 +28,10 @@ def openImage():
         #the [:] is neccessary to pass the list by value instead of reference, avoiding changing it.
         removeBlue(imageArray[:], size)
         stripBit(imageArray[:], size, 0)
+        encodeMessage(imageArray, size, text)
         
-def removeBlue(imageArray, size):   #set all blue values in pixels to 0 because I'm evil
+def removeBlue(imageArray, size):   
+    #set all blue values in pixels to 0 because I'm evil
     for i in range(len(imageArray)):
         r, g, b = imageArray[i]
         imageArray[i] = (r, g, 0)
@@ -42,7 +44,8 @@ def removeBlue(imageArray, size):   #set all blue values in pixels to 0 because 
     image2.putdata(imageArray)
     #image2.show()
 
-def stripBit(imageArray, size, bit):  #0 for bit is LSB
+def stripBit(imageArray, size, bit):  
+    #0 for bit is LSB
     for i in range(len(imageArray)):
         r, g, b = imageArray[i]
         imageArray[i] = setBit(r, bit, 0), setBit(g, bit, 0), setBit(b, bit, 0)
@@ -51,9 +54,21 @@ def stripBit(imageArray, size, bit):  #0 for bit is LSB
     #put imageData back into new image
     image2 = Image.new(mode="RGB", size=size)
     image2.putdata(imageArray)
+    #image2.show()
+    
+def encodeMessage(imageArray, size, message): 
+    #encodes a message into lsb of red pixels of a given image, message should be a string consisting of 0s and 1s.
+    #message must be shorter than imageArray, no checking so be careful!
+    for i in range(len(message)):
+        r, g, b = imageArray[i]
+        imageArray[i] = setBit(r, 0, int(message[i])), g, b
+        
+    print("\nData with encoded binary message: " + str(imageArray[0:10]))
+    #put imageData back into new image
+    image2 = Image.new(mode="RGB", size=size)
+    image2.putdata(imageArray)
     image2.show()
     
-
 
     
 #following 2 functions derived from https://wiki.python.org/moin/BitManipulation
@@ -68,13 +83,16 @@ def setBit(int_type, offset, value):
 def toggleBit(int_type, offset):
     mask = 1 << offset
     return(int_type ^ mask)
+    
+ 
 
 def main():
     parser = argparse.ArgumentParser(description = "Steganography encode/decode")
     parser.add_argument("-m", "--mode", dest = 'mode', type = str, required = True, help = "Mode of operation, e is encode and d is decode.")
+    parser.add_argument("-t", "--text", dest = 'text', type = str, required = True, help = "Message to encode, binary string")
     
     args = parser.parse_args()
-    printModeTest(args.mode)
+    printModeTest(args.mode, args.text)
     
     
 if __name__ == "__main__":
