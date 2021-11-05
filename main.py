5bbdd89a2c50b0060b7143c4a6782a8d8fd1b8fc
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2021-11-04 21:14:43 -0400

    make changes so that code actually works

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python
 
 #first argument is gonna be -m/mode e/encode/d/decode for Encode or Decode for now, then some sort of file input, gonna use a png image in local folder for now.
+
 import argparse
 import sys
 
 print("arguments:\t", sys.argv[1:], "\n")
 
-#argument = sys.argv[1]
-#if argument == "-d":
-#    print("decode mode")
-#elif argument == "-e":
-#    print("encode mode")
-    
-#print("start new stuff")
-    
 def printModeTest(mode):
-    print(mode)
-    if mode == "e" || mode == "encode":
+    if mode == "e" or mode == "encode":
         print("encode")
-    elif mode == "d" || mode == "decode":
+    elif mode == "d" or mode == "decode":
         print("decode")
-    
-    
-
 
 def main():
     parser = argparse.ArgumentParser(description = "Steganography encode/decode")
-    parser.add_argument("-m", "--mode", dest = mode, type = str, required = True, help = "Mode of operation, e is encode and d is decode.")
+    parser.add_argument("-m", "--mode", dest = 'mode', type = str, required = True, help = "Mode of operation, e is encode and d is decode.")
     
-    args = parse.parse_args()
+    args = parser.parse_args()
     printModeTest(args.mode)
     
     
