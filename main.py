0a11785f8223fe1608a56bd68dc5591d71474dc5
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2021-11-04 23:27:27 -0400

    add mode to strip LSB and supporting code

--- main.py
+++ main.py
@@ -22,27 +22,52 @@ def openImage():
         print("Total number of pixels: " + str(len(imageArray)))
         size = image.size
         print("Image size: " + str(size))
-        print("Inital data:   " + str(imageArray[0:10]))
+        print("Inital data: " + str(imageArray[0:10]))
         
-        removeBlue(imageArray, size)
-        
-
+        #call our two functions to modify the image data in different ways.
+        #the [:] is neccessary to pass the list by value instead of reference, avoiding changing it.
+        removeBlue(imageArray[:], size)
+        stripBit(imageArray[:], size, 0)
         
 def removeBlue(imageArray, size):   #set all blue values in pixels to 0 because I'm evil
-        for i in range(len(imageArray)):
-            r, g, b = imageArray[i]
-            imageArray[i] = (r, g, 0)
-            
-        #print out new data
-        print("\nModified data: " + str(imageArray[0:10]))
+    for i in range(len(imageArray)):
+        r, g, b = imageArray[i]
+        imageArray[i] = (r, g, 0)
+       
+    #print out new data
+    print("\nBlue data: " + str(imageArray[0:10]))
         
-        #put imageData back into new image
-        image2 = Image.new(mode="RGB", size=size)
-        image2.putdata(imageArray)
-        image2.show()
+    #put imageData back into new image
+    image2 = Image.new(mode="RGB", size=size)
+    image2.putdata(imageArray)
+    #image2.show()
+
+def stripBit(imageArray, size, bit):  #0 for bit is LSB
+    for i in range(len(imageArray)):
+        r, g, b = imageArray[i]
+        imageArray[i] = setBit(r, bit, 0), setBit(g, bit, 0), setBit(b, bit, 0)
+        
+    print("\nData without LSB: " + str(imageArray[0:10]))
+    #put imageData back into new image
+    image2 = Image.new(mode="RGB", size=size)
+    image2.putdata(imageArray)
+    image2.show()
     
-            
+
+
+    
+#following 2 functions derived from https://wiki.python.org/moin/BitManipulation
+def setBit(int_type, offset, value):
+    if value == 1:
+        mask = 1 << offset
+        return(int_type | mask)
+    if value == 0:
+        mask = ~(1 << offset)
+        return(int_type & mask)
         
+def toggleBit(int_type, offset):
+    mask = 1 << offset
+    return(int_type ^ mask)
 
 def main():
     parser = argparse.ArgumentParser(description = "Steganography encode/decode")
