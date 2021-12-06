57927a2c5a335a06f8bcf496a437701ec1894665
Brendan Fraser <73411747+brendanfraser597@users.noreply.github.com>
2021-12-06 00:33:45 -0500

    start loop for sending and recieving actual messages

--- controlPanel.py
+++ controlPanel.py
@@ -9,36 +9,45 @@ msg = ""
 key = ""
 steg.doLogOutput = False
 
-active = True
-while(active):
-    mode = input("Welcome!\nEnter 1 to resume an existing session.\nEnter 2 to start a new session.\nEnter 3 to quit.\n")
-    if mode == "1":
-        key = diffieHellman.resumeCommunicationSession()
-        print(f"Key: {key}")
-    elif mode == "2":
-        keyToShare = diffieHellman.startNewCommunication().decode("ascii")
-        #construct file name to look like camera picture from smartphone, using current date and time
-        now = datetime.now()
-        str = now.strftime("./media/IMG_%Y%m%d_%H%M%S.png")
-        steg.encodeMessageIntoImage(keyToShare, "./media/eyes.png", str)
-        print(f"\nKey encoded into inital image stored at {str}\n")
-        input("Waiting for shared inital image from the other party to be put into the ./input folder.\nPlease press enter once the image is put there.")
+def mainLoop():
+    active = True
+    while(active):
+        mode = input("Welcome!\nEnter 1 to resume an existing session.\nEnter 2 to start a new session.\nEnter 3 to quit.\n")
+        if mode == "1":
+            global key
+            key = diffieHellman.resumeCommunicationSession()
+            print(f"Key: {key}")
+        elif mode == "2":
+            keyToShare = diffieHellman.startNewCommunication().decode("ascii")
+            #construct file name to look like camera picture from smartphone, using current date and time
+            now = datetime.now()
+            str = now.strftime("./media/IMG_%Y%m%d_%H%M%S.png")
+            steg.encodeMessageIntoImage(keyToShare, "./media/eyes.png", str)
+            print(f"\nKey encoded into inital image stored at {str}\n")
+            input("Waiting for shared inital image from the other party to be put into the ./input folder.\nPlease press enter once the image is put there.")
 
-        #decode information from newest image located in ./input
-        #code to get newest taken from https://stackoverflow.com/questions/39327032/how-to-get-the-latest-file-in-a-folder
-        print("\nGetting newest image in ./input....")
+            #decode information from newest image located in ./input
+            #code to get newest taken from https://stackoverflow.com/questions/39327032/how-to-get-the-latest-file-in-a-folder
+            print("\nGetting newest image in ./input....")
+            file = max([os.path.join("./input/", basename) for basename in os.listdir("./input/")], key=os.path.getctime)
+            print(f"\nFile found! Filename: {file}")
+            sharedKey = bytes(steg.decodeMessageFromImage(file), "utf8")
+            global key
+            key = diffieHellman.receiveExternalKey(sharedKey)
+            print(f"Key: {key}")
+        elif mode == "3":
+            active = False
+        else:
+            print("Invalid input, please try again.")
+        
+        
+def recieveSendMessagesLoop():
+    active = True
+    mode = input("Press 1 to recieve a message, 2 to send a message, or 3 to go back.")
+    if mode == "1":
+        input("Waiting for image to be put into ./input, newst file will be seleted!")
         file = max([os.path.join("./input/", basename) for basename in os.listdir("./input/")], key=os.path.getctime)
         print(f"\nFile found! Filename: {file}")
-        sharedKey = bytes(steg.decodeMessageFromImage(file), "utf8")
-        key = diffieHellman.receiveExternalKey(sharedKey)
-        print(f"Key: {key}")
-    elif mode == "3":
-        active = False
-    else:
-        print("Invalid input, please try again.")
-        
-        
-        
 
 
 """
