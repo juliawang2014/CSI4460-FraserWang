97ed5f112df54e94e68bf06cf2962deb99df3f1d
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2021-12-06 11:35:29 -0500

    add handling for putting actual message into steg and getting it back out!!!!!!!!!!!!!!!!!!

--- controlPanel.py
+++ controlPanel.py
@@ -1,52 +1,75 @@
 import libraries.diffieHellman as diffieHellman
-import libraries.AES as AES
 import libraries.steg as steg
 from datetime import datetime
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 import os
 
-ivector = ""
+ivector = b'\xd4\xef[y2\x91\x0c\x14\ry\x88\x13\xc4$\xfd&'
 msg = ""
 key = ""
 steg.doLogOutput = False
 
 def mainLoop():
-    active = True
-    while(active):
-        mode = input("Welcome!\nEnter 1 to resume an existing session.\nEnter 2 to start a new session.\nEnter 3 to quit.\n")
-        if mode == "1":
-            global key
-            key = diffieHellman.resumeCommunicationSession()
-            print(f"Key: {key}")
-        elif mode == "2":
-            keyToShare = diffieHellman.startNewCommunication().decode("ascii")
-            #construct file name to look like camera picture from smartphone, using current date and time
-            now = datetime.now()
-            str = now.strftime("./media/IMG_%Y%m%d_%H%M%S.png")
-            steg.encodeMessageIntoImage(keyToShare, "./media/eyes.png", str)
-            print(f"\nKey encoded into inital image stored at {str}\n")
-            input("Waiting for shared inital image from the other party to be put into the ./input folder.\nPlease press enter once the image is put there.")
+    mode = input("Welcome!\nEnter 1 to resume an existing session.\nEnter 2 to start a new session.\nEnter 3 to quit.\n")
+    if mode == "1":
+        global key
+        key = diffieHellman.resumeCommunicationSession()
+        print(f"Key: {key}")
+        recieveSendMessagesLoop()
+    elif mode == "2":
+        keyToShare = diffieHellman.startNewCommunication().decode("ascii")
+        #construct file name to look like camera picture from smartphone, using current date and time
+        now = datetime.now()
+        str = now.strftime("./output/IMG_%Y%m%d_%H%M%S.png")
+        steg.encodeMessageIntoImage(keyToShare, "./media/eyes.png", str)
+        print(f"\nKey encoded into inital image stored at {str}\n")
+        input("Waiting for shared inital image from the other party to be put into the ./input folder.\nPlease press enter once the image is put there.")
 
-            #decode information from newest image located in ./input
-            #code to get newest taken from https://stackoverflow.com/questions/39327032/how-to-get-the-latest-file-in-a-folder
-            print("\nGetting newest image in ./input....")
-            file = max([os.path.join("./input/", basename) for basename in os.listdir("./input/")], key=os.path.getctime)
-            print(f"\nFile found! Filename: {file}")
-            sharedKey = bytes(steg.decodeMessageFromImage(file), "utf8")
-            key = diffieHellman.receiveExternalKey(sharedKey)
-            print(f"Key: {key}")
-        elif mode == "3":
-            active = False
-        else:
-            print("Invalid input, please try again.")
+        #decode information from newest image located in ./input
+        #code to get newest taken from https://stackoverflow.com/questions/39327032/how-to-get-the-latest-file-in-a-folder
+        print("\nGetting newest image in ./input....")
+        file = max([os.path.join("./input/", basename) for basename in os.listdir("./input/")], key=os.path.getctime)
+        print(f"\nFile found! Filename: {file}")
+        sharedKey = bytes(steg.decodeMessageFromImage(file), "utf8")
+        key = diffieHellman.receiveExternalKey(sharedKey)
+        print(f"Key: {key}")
+        recieveSendMessagesLoop()
+    elif mode == "3":
+        print("Goodbye!")
+    else:
+        print("Invalid input, please try again.")
+        mainLoop()
         
         
 def recieveSendMessagesLoop():
-    active = True
-    mode = input("Press 1 to recieve a message, 2 to send a message, or 3 to go back.")
+    mode = input("\nPress 1 to recieve a message, 2 to send a message, or 3 to go back.\n")
     if mode == "1":
-        input("Waiting for image to be put into ./input, newst file will be seleted!")
+        cipher = AES.new(key,AES.MODE_CBC, iv=ivector)
+        input("Waiting for image to be put into ./input, newest file will be seleted\nPlease press enter once the image is put there.\n")
         file = max([os.path.join("./input/", basename) for basename in os.listdir("./input/")], key=os.path.getctime)
-        print(f"\nFile found! Filename: {file}")
+        print(f"File found! Filename: {file}")
+        keyForSteg = key.hex().upper()
+        messageEncrypted = steg.decodeMessageFromImage(file, inputKey=keyForSteg)
+        messageDecrypted = unpad(cipher.decrypt(messageEncrypted), AES.block_size)
+        print(f"Message decoded from file:\n{messageDecrypted.decode('ascii')}")
+        recieveSendMessagesLoop()
+    elif mode == "2":
+        cipher = AES.new(key,AES.MODE_CBC, iv=ivector)
+        messageToEncode = input("Please enter your message now!\n")
+        messageEncrypted = cipher.encrypt(pad(bytes(messageToEncode, "ascii"), AES.block_size))
+        #construct file name to look like camera picture from smartphone, using current date and time
+        now = datetime.now()
+        str = now.strftime("./output/IMG_%Y%m%d_%H%M%S.png")
+        keyForSteg = key.hex().upper()
+        steg.encodeMessageIntoImage(messageEncrypted, "./media/eyes.png", str, inputKey=keyForSteg)
+        print(f"\nMessage encoded into image stored at {str}")
+        recieveSendMessagesLoop()
+    elif mode == "3":
+        mainLoop()
+    else:
+        print("Invalid input, please try again.")
+        recieveSendMessagesLoop()
 
 
 """
