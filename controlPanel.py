2348818402f2fcbb3df60db72900b5ea65944866
brendanfraser597 <73411747+brendanfraser597@users.noreply.github.com>
2021-12-07 02:06:45 -0500

    document + clean up controlPanel.py

--- controlPanel.py
+++ controlPanel.py
@@ -6,31 +6,34 @@ from Crypto.Util.Padding import pad, unpad
 import os
 import secrets
 
-msg = ""
 key = ""
 steg.doLogOutput = False
 
 def mainLoop():
     mode = input("Welcome!\nEnter 1 to resume an existing session.\nEnter 2 to start a new session.\nEnter 3 to quit.\n")
     if mode == "1":
+        #load paramaters from paramaters.config to resume previous session that has already been set up
         global key
         key = diffieHellman.resumeCommunicationSession()
         print(f"Key: {key}")
         recieveSendMessagesLoop()
     elif mode == "2":
+        #initialize diffieHellman process
         keyToShare = diffieHellman.startNewCommunication().decode("ascii")
         #construct file name to look like camera picture from smartphone, using current date and time
         now = datetime.now()
         str = now.strftime("./output/IMG_%Y%m%d_%H%M%S.png")
+        #encode public key into image
+        #TODO: pick random image from ./media/ to encode
         steg.encodeMessageIntoImage(keyToShare, "./media/eyes.png", str)
         print(f"\nKey encoded into inital image stored at {str}\n")
         input("Waiting for shared inital image from the other party to be put into the ./input folder.\nPlease press enter once the image is put there.")
-
         #decode information from newest image located in ./input
         #code to get newest taken from https://stackoverflow.com/questions/39327032/how-to-get-the-latest-file-in-a-folder
         print("\nGetting newest image in ./input....")
         file = max([os.path.join("./input/", basename) for basename in os.listdir("./input/")], key=os.path.getctime)
         print(f"\nFile found! Filename: {file}")
+        #get public key encoded in image and derive session key from the public key
         sharedKey = bytes(steg.decodeMessageFromImage(file), "utf8")
         key = diffieHellman.receiveExternalKey(sharedKey)
         print(f"Key: {key}\nCurrent session has been saved.")
@@ -41,23 +44,25 @@ def mainLoop():
         print("Invalid input, please try again.")
         mainLoop()
         
-        
 def recieveSendMessagesLoop():
     mode = input("\nPress 1 to recieve a message, 2 to send a message, or 3 to go back.\n")
     if mode == "1":
-
+        #get newest file in ./input, which should contain the ivector + encrypted message
         input("Waiting for image to be put into ./input, newest file will be seleted\nPlease press enter once the image is put there.\n")
         file = max([os.path.join("./input/", basename) for basename in os.listdir("./input/")], key=os.path.getctime)
         print(f"File found! Filename: {file}")
+        #extract ivector and encrypted message
         keyForSteg = key.hex().upper()
         messageEncoded = steg.decodeMessageFromImage(file, inputKey=keyForSteg)
         ivector = messageEncoded[0:16]
         messageEncrypted = messageEncoded[16:]
+        #create cipher and decrpt message
         cipher = AES.new(key,AES.MODE_CBC, iv=ivector)
         messageDecrypted = unpad(cipher.decrypt(messageEncrypted), AES.block_size)
         print(f"Message decoded from file:\n{messageDecrypted.decode('ascii')}")
         recieveSendMessagesLoop()
     elif mode == "2":
+        #initalize AES encryption with cryptographically secure random ivector
         ivector = secrets.token_bytes(16)
         cipher = AES.new(key,AES.MODE_CBC, iv=ivector)
         messageToEncode = input("Please enter your message now!\n")
@@ -66,6 +71,8 @@ def recieveSendMessagesLoop():
         now = datetime.now()
         str = now.strftime("./output/IMG_%Y%m%d_%H%M%S.png")
         keyForSteg = key.hex().upper()
+        #encode into ./media/eyes.png 
+        #TODO: put many pictures into ./media and choose randomly from them
         steg.encodeMessageIntoImage(ivector + messageEncrypted, "./media/eyes.png", str, inputKey=keyForSteg)
         print(f"\nMessage encoded into image stored at {str}")
         recieveSendMessagesLoop()
@@ -77,32 +84,3 @@ def recieveSendMessagesLoop():
 
 if __name__ == "__main__":
     mainLoop()
-
-
-"""
-while(active):
-    mode = input("Welcome, enter 1 to encrypt and send a new message, enter 2 to receive and decrypt a message, enter 3 to quit\n")
-    if mode == "1":
-        message = input("Input message to send: ")
-        notSent = True
-        while(notSent):
-            choice = input("Enter 1 to add AES encryption, enter 2 to send the message using steganography, enter 3 to go back\n")
-            if choice == "1":
-                key = diffieHellman.getSharedKey()
-                m, iv = AES.encryption(message, key)
-                ivector = iv
-                msg = message
-                print("Cipher text: {}".format(msg))
-            elif choice == "2":
-                notSent = False
-            elif choice == "3":
-                notSent = False
-            else:
-                print("Invalid input, try again")
-    elif mode == "2":
-        msg = AES.decryption(msg, key, ivector)
-        print("Decrypted message: {}".format(msg))
-    elif mode == "3":
-        active = False
-    else:
-        print("Invalid input, try again.")"""
